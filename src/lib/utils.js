ccac8a7bd727eecda005c3cf8ad0f7464f2838e5
Oussama Bof <jackmarrow06@gmail.com>
2024-03-09 16:53:08 +0100

    Date format and salary format has been added

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,32 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function salaryFormat(salary) {
+  return parseInt(salary).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+}
+
+export function dateFormat(dateOfCreation) {
+  const currentDate = parseInt(new Date().getTime() / 1000); //By seconds
+  const timeDifference = currentDate - dateOfCreation;
+  if (timeDifference < 60) {
+    return (`${timeDifference} second ago`);
+  } else if (timeDifference < 3600) {
+    const result = parseInt(timeDifference / 60);
+    return (`${result} minute(s) ago`);
+  } else if (timeDifference < 86400) {
+    const result = parseInt(timeDifference / 3600);
+    return (`${result} hour(s) ago`);
+  } else {
+    const result = parseInt(timeDifference / 86400);
+    return (`${result} day(s) ago`);
+  }
+}
+
+
